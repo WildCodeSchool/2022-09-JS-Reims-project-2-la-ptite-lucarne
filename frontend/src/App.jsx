78f631805afab70db207297ce1e36cfbffcde4eb
ROBIN KOLASINSKI <robin.kolasinski@gmail.com>
2022-11-09 14:03:25 +0100

    Added filters and button acting on filters

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,16 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Movie from "./components/Movie";
 import Filter from "./components/Filter";
-//import Config from "./Config";
-
+// import Config from "./Config";
 
 // récupère une partie déjà filtrée de l'API
 
 function App() {
-  
-  const [genre,setGenre] = useState(" ")
-    const [myUrl, setMyUrl] = useState(`https://api.themoviedb.org/3/discover/movie?with_genre=${genre}&api_key=f365f4ddf79f3707857efed734c40500&language=fr&page=`);
-    const [filtered, setFiltered] = useState([]);
-    
+  const [genre, setGenre] = useState(" ");
+  const [myUrl, setMyUrl] = useState(
+    `https://api.themoviedb.org/3/discover/movie?with_genre=${genre}&api_key=f365f4ddf79f3707857efed734c40500&language=fr&page=`
+  );
+  const [filtered, setFiltered] = useState([]);
 
   // récupère les données de l'url et les affectent au states correspondants
   const fetchPopular = async () => {
@@ -22,7 +21,6 @@ function App() {
     setFiltered(movies.results);
   };
 
-  
   useEffect(() => {
     fetchPopular();
   }, [myUrl]);
@@ -30,14 +28,13 @@ function App() {
   return (
     <div className="App">
       <h1>Movies</h1>
-      <p>
-        Filtres actuels : Par popularité</p>
+      <p>Filtres actuels : Par popularité</p>
       <Filter
         setFiltered={setFiltered}
         setMyUrl={setMyUrl}
         setGenre={setGenre}
         genre={genre}
-        />
+      />
       <div className="popular-movies">
         {filtered.map((movie) => {
           return <Movie key={movie.id} movie={movie} />;
