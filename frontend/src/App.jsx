3cd72c7f012890a4b068ce8eb5288ff91a7f267d
Olif51 <99129655+Olif51@users.noreply.github.com>
2022-11-16 11:11:20 +0100

    import question component to app in static way, buttons not functionnal all app.jsx deleted to replace with choice process

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,84 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-
-import Movie from "./pages/Movie";
-import Filter from "./components/Filter";
-import Home from "./pages/Home";
 import Question from "./components/Question";
-import VisualDetails from "./pages/VisualDetails";
-import Visual from "./pages/Visual";
 
 function App() {
-  const [myUrl, setMyUrl] = useState(
-    `https://api.themoviedb.org/3/discover/movie&api_key=f365f4ddf79f3707857efed734c40500&language=fr&page=`
-  );
-
-  const [filtered, setFiltered] = useState([]);
-  const [dejavu, setDejavu] = useState(0);
-  const [currentPage, setCurrentPage] = useState("movie");
-
-  const fetchPopular = async () => {
-    const data = await fetch(myUrl);
-    const movies = await data.json();
-    setFiltered(movies.results);
-  };
-
-  useEffect(() => {
-    fetchPopular();
-  }, [myUrl]);
-
   return (
-    <>
-      <div className="App">
-        <h1>Movies</h1>
-        <p>Filtres actuels : Par popularité</p>
-        <Filter
-          setFiltered={setFiltered}
-          setMyUrl={setMyUrl}
-          setGenre={setGenre}
-          genre={genre}
-        />
-        <div className="popular-movies">
-          {filtered[dejavu] != null && (
-            <Movie
-              setDejavu={setDejavu}
-              dejavu={dejavu}
-              movie={filtered[dejavu]}
-              movieId={filtered[dejavu].id}
-              movieBackdropPath={filtered[dejavu].backdrop_path}
-              moviePath={filtered[dejavu].path}
-              movieTitle={filtered[dejavu].title}
-              movieOverview={filtered[dejavu].overview}
-            />
-          )}
-        </div>
-        {show === "home" && <Home setShow={setShow} nextTitle="test" />}
-      </div>
-      <div>
-        <p>
-          <Question />
-        </p>
-      </div>
-    </>
     <div>
-      {currentPage === "movie" && (
-        <Visual
-          setCurrentPage={setCurrentPage}
-          dejavu={dejavu}
-          setDejavu={setDejavu}
-          filtered={filtered}
-          setMyUrl={setMyUrl}
-        />
-      )}
-      {currentPage === "movieDetails" && (
-        <VisualDetails
-          setCurrentPage={setCurrentPage}
-          dejavu={dejavu}
-          setDejavu={setDejavu}
-          filtered={filtered}
-          setMyUrl={setMyUrl}
-        />
-      )}
+      <p>
+        <Question />
+      </p>
     </div>
   );
 }
